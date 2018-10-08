9f4b7c6b4bce83bf34626fdc3d6e0b656cd1a6af
Dmitry Vorotilin <d.vorotilin@gmail.com>
2018-10-08 17:19:31 +0300

    Wrap in ()

--- lib/capybara/cuprite/node.rb
+++ lib/capybara/cuprite/node.rb
@@ -39,20 +39,20 @@ module Capybara::Cuprite
     end
 
     def find_xpath(selector)
-      find :xpath, selector
+      find(:xpath, selector)
     end
 
     def find_css(selector)
-      find :css, selector
+      find(:css, selector)
     end
 
     def all_text
-      filter_text command(:all_text)
+      filter_text(command(:all_text))
     end
 
     def visible_text
       if Capybara::VERSION.to_f < 3.0
-        filter_text command(:visible_text)
+        filter_text(command(:visible_text))
       else
         command(:visible_text).to_s
                               .gsub(/\A[[:space:]&&[^\u00a0]]+/, "")
@@ -63,7 +63,7 @@ module Capybara::Cuprite
     end
 
     def property(name)
-      command :property, name
+      command(:property, name)
     end
 
     def [](name)
@@ -81,11 +81,11 @@ module Capybara::Cuprite
     end
 
     def attributes
-      command :attributes
+      command(:attributes)
     end
 
     def value
-      command :value
+      command(:value)
     end
 
     def set(value, options = {})
@@ -99,20 +99,20 @@ module Capybara::Cuprite
           click if value != checked?
         when "file"
           files = value.respond_to?(:to_ary) ? value.to_ary.map(&:to_s) : value.to_s
-          command :select_file, files
+          command(:select_file, files)
         else
-          command :set, value.to_s
+          command(:set, value.to_s)
         end
       elsif tag_name == "textarea"
-        command :set, value.to_s
+        command(:set, value.to_s)
       elsif self[:isContentEditable]
-        command :delete_text
+        command(:delete_text)
         send_keys(value.to_s)
       end
     end
 
     def select_option
-      command :select, true
+      command(:select, true)
     end
 
     def unselect_option
@@ -125,7 +125,7 @@ module Capybara::Cuprite
     end
 
     def visible?
-      command :visible?
+      command(:visible?)
     end
 
     def checked?
@@ -137,35 +137,35 @@ module Capybara::Cuprite
     end
 
     def disabled?
-      command :disabled?
+      command(:disabled?)
     end
 
     def click(keys = [], offset = {})
-      command :click, keys, offset
+      command(:click, keys, offset)
     end
 
     def right_click(keys = [], offset = {})
-      command :right_click, keys, offset
+      command(:right_click, keys, offset)
     end
 
     def double_click(keys = [], offset = {})
-      command :double_click, keys, offset
+      command(:double_click, keys, offset)
     end
 
     def hover
-      command :hover
+      command(:hover)
     end
 
     def drag_to(other)
-      command :drag, other.node
+      command(:drag, other.node)
     end
 
     def drag_by(x, y)
-      command :drag_by, x, y
+      command(:drag_by, x, y)
     end
 
     def trigger(event)
-      command :trigger, event
+      command(:trigger, event)
     end
 
     def ==(other)
@@ -176,12 +176,12 @@ module Capybara::Cuprite
     end
 
     def send_keys(*keys)
-      command :send_keys, keys
+      command(:send_keys, keys)
     end
     alias_method :send_key, :send_keys
 
     def path
-      command :path
+      command(:path)
     end
 
     def inspect
@@ -190,7 +190,7 @@ module Capybara::Cuprite
 
     # @api private
     def to_json(*)
-      JSON.generate as_json
+      JSON.generate(as_json)
     end
 
     # @api private
