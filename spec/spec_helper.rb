4038d4b72cc29f1bcffd70f1939fce75fd06f8ec
Dmitry Vorotilin <d.vorotilin@gmail.com>
2019-01-08 19:22:39 +0300

    Already works

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -87,7 +87,6 @@ RSpec.configure do |config|
     #go_forward should fetch a response from the driver from the previous page
     #save_page asset_host contains a string prepends base tag with value from asset_host to the head
     #save_page asset_host contains a string doesn't prepend base tag to pages when asset_host is nil
-    #has_no_text? with wait should not find element if it appears after given wait duration
     #within_window with an instance of Capybara::Window should leave correct scopes after execution in case of error
     #fullscreen should be able to fullscreen the window
     #click_link can download a file
