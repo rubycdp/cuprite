2fe03fe33f7d2bca1d59964c84d5527b5de7d07e
Dmitry Vorotilin <d.vorotilin@gmail.com>
2019-10-30 13:51:21 +0300

    Fix #102 no more monkey patching

--- lib/capybara/cuprite.rb
+++ lib/capybara/cuprite.rb
@@ -2,17 +2,11 @@
 
 require "ferrum"
 require "capybara"
-
-module Capybara::Cuprite
-end
-
 require "capybara/cuprite/driver"
 require "capybara/cuprite/browser"
 require "capybara/cuprite/page"
 require "capybara/cuprite/node"
 require "capybara/cuprite/errors"
-
-Ferrum::Page.prepend(Capybara::Cuprite::Page)
 
 Capybara.register_driver(:cuprite) do |app|
   Capybara::Cuprite::Driver.new(app)
