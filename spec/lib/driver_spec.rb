12e34ec8c3b7af53de5d9dc2cbb4c3ef71e8176c
Oli Peate <oli.peate@freeagent.com>
2022-05-11 09:52:49 +0100

    Support custom save_path option
    Allows paths other than Capybara.save_path to be specified

---
+++ spec/lib/driver_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+describe Capybara::Cuprite::Driver do
+  describe "save_path configuration" do
+    it "defaults to the Capybara save path" do
+      driver = with_capybara_save_path("/tmp/capybara-save-path") do
+        described_class.new(nil)
+      end
+
+      expect(driver.browser.options).to include(save_path: "/tmp/capybara-save-path")
+    end
+
+    it "allows a custom path to be specified" do
+      custom_path = Dir.mktmpdir
+
+      driver = with_capybara_save_path("/tmp/capybara-save-path") do
+        described_class.new(nil, { save_path: custom_path })
+      end
+
+      expect(driver.browser.options).to include(save_path: custom_path)
+    end
+  end
+
+  private
+
+  def with_capybara_save_path(path)
+    original_capybara_save_path = Capybara.save_path
+    Capybara.save_path = path
+    yield
+  ensure
+    Capybara.save_path = original_capybara_save_path
+  end
+end
