b4eeaf2e06ae6882e0e7b3046fb5ead39725c408
Ivan Kuchin <tadump+git@gmail.com>
2025-01-08 14:56:44 +0100

    scroll to destination when dragging (#288)

--- lib/capybara/cuprite/browser.rb
+++ lib/capybara/cuprite/browser.rb
@@ -139,26 +139,33 @@ module Capybara
         raise NotImplementedError
       end
 
-      def drag(node, other, steps, delay = nil)
+      def drag(node, other, steps, delay = nil, scroll = true)
         x1, y1 = node.find_position
-        x2, y2 = other.find_position
 
         mouse.move(x: x1, y: y1)
         mouse.down
         sleep delay if delay
+
+        other.scroll_into_view if scroll
+
+        x2, y2 = other.find_position
         mouse.move(x: x2, y: y2, steps: steps)
+
         mouse.up
       end
 
-      def drag_by(node, x, y, steps, delay = nil)
+      def drag_by(node, dx, dy, steps, delay = nil, scroll = true)
         x1, y1 = node.find_position
-        x2 = x1 + x
-        y2 = y1 + y
 
         mouse.move(x: x1, y: y1)
         mouse.down
+
         sleep delay if delay
-        mouse.move(x: x2, y: y2, steps: steps)
+
+        evaluate("window.scrollBy(#{dx}, #{dy})") if scroll # should be extracted to Mouse#scroll_by in ferrum
+
+        x2, y2 = node.find_position
+        mouse.move(x: x2 + dx, y: y2 + dy, steps: steps)
         mouse.up
       end
 
