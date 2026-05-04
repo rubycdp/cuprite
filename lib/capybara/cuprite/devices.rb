b4e8fef047c27ef137f87bca5c12d8a49dd80f4a
Duncan Bayne <duncan@bayne.id.au>
2026-05-04 15:45:17 +1000

    Fixup Rubocop warnings

--- lib/capybara/cuprite/devices.rb
+++ lib/capybara/cuprite/devices.rb
@@ -3,7 +3,9 @@
 module Capybara
   module Cuprite
     module Devices
-      IPHONE_14 = { window_size: [390, 844], mobile: true, scale_factor: 3 }
+      # It's idiomatic to merge other options into these, so freezing
+      # isn't the right thing to do.
+      IPHONE_14 = { window_size: [390, 844], mobile: true, scale_factor: 3 } # rubocop:disable Style/MutableConstant
     end
   end
 end
